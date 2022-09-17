7d99e030dfeed0208cb71775ae85de0dc8056274
florent <florent@traktor>
2022-09-17 18:33:55 +0200

    Aménagement tableau de bord

--- admin/spending.php
+++ admin/spending.php
@@ -38,22 +38,62 @@ $doc_title = 'Dépenses';
 			$now = new DateTime();
 			$currentYear = $now->format('Y');
 			$previousYear = $currentYear-1;
+			
+			$progress_rate = array();
+			$progress_amount = array();
+			
+			
 			foreach ($stats as $tag=>$amounts) {
-				echo '<div class="col-lg-2">';
+				if ($amounts[$previousYear]!=0) {
+					$progress_rate [$tag] = round(((($amounts[$currentYear]-$amounts[$previousYear])/$amounts[$previousYear]))*100,1);
+					$progress_amount [$tag] = round($amounts[$currentYear]-$amounts[$previousYear],2);
+				}
+			}
+			arsort($progress_amount);
+			echo '<div class="col-lg-6">';
+			echo '<h2>En augmentation</h2>';
+			echo '<p><small>Par rapport à '.$previousYear.', à la même date</small></p>';
+			echo '<div class="row">';
+			foreach ($progress_amount as $tag=>$value) {
+				if ($value<=0) {
+					break;
+				}
+				echo '<div class="col-lg-4">';
 				echo '<div class="card text-center mb-2">';
 				echo '<div class="card-body">';
 				echo '<a href="tag.php?label='.urlencode($tag).'"><h5 class="card-title">'.ToolBox::toHtml($tag).'</h5></a>';
-				echo '<div><big>'.$system->formatAmountToDisplay($amounts[$currentYear]).'</big></div>';
-				if ($amounts[$previousYear]!=0) {
-					echo '<div><small>('.round(((($amounts[$currentYear]-$amounts[$previousYear])/$amounts[$previousYear])*100),1).'%)</small></div>';
-				}
+				echo '<div><big>+'.$system->formatAmountToDisplay($progress_amount[$tag]).'</big></div>';
+				echo '<div><small>(soit +'.$progress_rate[$tag].'%)</small></div>';
 				echo '</div>';
 				echo '</div>';
 				echo '</div>';
 			}
+			echo '</div>';
+			echo '</div>';
+			
+			asort($progress_amount);
+			echo '<div class="col-lg-6">';
+			echo '<h3>En diminution</h3>';
+			echo '<p><small>Par rapport à '.$previousYear.', à la même date</small></p>';
+			echo '<div class="row">';
+			foreach ($progress_amount as $tag=>$value) {
+				if ($value>=0) {
+					break;
+				}
+				echo '<div class="col-lg-4">';
+				echo '<div class="card text-center mb-2">';
+				echo '<div class="card-body">';
+				echo '<a href="tag.php?label='.urlencode($tag).'"><h5 class="card-title">'.ToolBox::toHtml($tag).'</h5></a>';
+				echo '<div><big>'.$system->formatAmountToDisplay($progress_amount[$tag]).'</big></div>';
+				echo '<div><small>(soit '.$progress_rate[$tag].'%)</small></div>';
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
+			}
+			echo '</div>';
+			echo '</div>';
 		?>
 		</div>
-		
 		</main>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>	
